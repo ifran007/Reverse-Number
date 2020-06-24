f19239f31dca105a45812a078202a679c95f6bf5
Mohammad Ifran Uddin <67389212+ifran007@users.noreply.github.com>
2020-06-25 03:09:27 +0600

    Add files via upload

---
+++ ReverseDemo.java
@@ -0,0 +1,25 @@
+
+package oop;
+
+import java.util.Scanner;
+
+public class ReverseDemo {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int num, sum = 0, r, temp;
+        System.out.println("Enter your number: ");
+        num = input.nextInt();
+        temp = num;
+        
+            while(temp != 0)
+        {
+            r = temp % 10;
+            sum = sum * 10 + r;
+            temp = temp / 10;
+
+        }
+        System.out.println("Reverse number : "+sum);
+    }
+}
